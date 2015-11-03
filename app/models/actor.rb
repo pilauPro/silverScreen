1e3d6330f189e780fee812dc268c6d5d0e4d574d
pilauPro <leiningem001@hawaii.rr.com>
2015-11-03 09:49:31 -1000

    validations for Actor and Movie

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,6 @@
 class Actor < ActiveRecord::Base
 	has_many :roles
 	has_many :movies, through: :roles
+
+	validates_presence_of :fname, :lname
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ActiveRecord::Base
 	has_many :roles
 	has_many :actors, through: :roles
+
+	validates_presence_of :title, :year
 end
