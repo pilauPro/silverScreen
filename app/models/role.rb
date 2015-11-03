c71b318ba4459e684870f69a294d4b3fc92692d8
pilauPro <leiningem001@hawaii.rr.com>
2015-11-03 10:09:22 -1000

    added name to Role

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,5 +2,5 @@ class Role < ActiveRecord::Base
   belongs_to :movie
   belongs_to :actor
 
-  validates_presence_of :movie, :actor
+  validates_presence_of :movie, :actor, :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103192428) do
+ActiveRecord::Schema.define(version: 20151103200821) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "fname"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20151103192428) do
     t.integer  "actor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   add_index "roles", ["actor_id"], name: "index_roles_on_actor_id"
