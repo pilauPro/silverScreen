ebe3e67423fbcb9a8b65b24b50d7ccb98108f87b
pilauPro <leiningem001@hawaii.rr.com>
2015-11-03 12:12:25 -1000

    formatting select boxes to display alpha order and concatenate full name for actor

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,9 @@
 class Actor < ActiveRecord::Base
 	has_many :roles
 	has_many :movies, through: :roles
-
 	validates_presence_of :fname, :lname
+
+	def full_name
+		"#{lname}, #{fname}"
+	end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -3,4 +3,5 @@ class Role < ActiveRecord::Base
   belongs_to :actor
 
   validates_presence_of :movie, :actor, :name
+  validates :movie_id, :uniqueness => {:scope => :actor_id}
 end
