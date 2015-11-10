fd6b76df497358ce7c00c9e433a886af5b6a1759
pilauPro <leiningem001@hawaii.rr.com>
2015-11-09 17:54:28 -1000

    description column added to roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103200821) do
+ActiveRecord::Schema.define(version: 20151110035221) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "fname"
@@ -35,9 +35,10 @@ ActiveRecord::Schema.define(version: 20151103200821) do
   create_table "roles", force: :cascade do |t|
     t.integer  "movie_id"
     t.integer  "actor_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "name"
+    t.text     "description"
   end
 
   add_index "roles", ["actor_id"], name: "index_roles_on_actor_id"
